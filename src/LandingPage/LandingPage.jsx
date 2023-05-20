64c310267fa3386830ee5e8f3291001d727fd79d
Viekesh <vikeshgaikwad1@gmail.com>
2023-05-20 17:20:08 +0530

    Initial Commit

---
+++ src/LandingPage/LandingPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Nav from '../Components/Navigation/Nav';
+
+const LandingPage = () => {
+  return (
+    <div>
+      <Nav />
+    </div>
+  )
+}
+
+export default LandingPage;
+
+
---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import LandingPage from '../LandingPage/LandingPage';
+
+const Routes = () => {
+    return (
+        <>
+            <LandingPage />
+        </>
+    )
+}
+
+export default Routes;
+
+
