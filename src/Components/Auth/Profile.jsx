201d862516ff482f56c99d0c64029387384bfa33
Viekesh <vikeshgaikwad1@gmail.com>
2023-06-30 18:22:59 +0530

    Continue To Code On Profile Component To Fetch CreateList Data

--- src/Components/Auth/Profile.jsx
+++ src/Components/Auth/Profile.jsx
@@ -1,25 +1,71 @@
-import React, { useState } from 'react'
-import { auth } from '../../FirebaseConfig';
+import React, { useEffect, useState } from 'react'
+import { auth, database } from '../../FirebaseConfig';
 import { NavLink, useNavigate } from 'react-router-dom';
 import "./Profile.scss";
 import Nav from '../Navigation/Nav';
+import { collection, getDoc, orderBy, query, where } from 'firebase/firestore';
+
+
 
 const Profile = () => {
+
+  // here we can use useState hook to fetch data from firestore database
+  const [listings, setListings] = useState(null);
+
+
+  const [loading, setLoading] = useState(true);
+
 
   const [formData, setFormData] = useState({
     firstname: auth.currentUser.displayName,
     email: auth.currentUser.email,
   });
 
+
   const { firstname, email } = formData;
 
+
   const afterSignOut = useNavigate();
+
 
   const onLogOut = () => {
     auth.signOut();
     afterSignOut("/");
     alert("You Are Successfully Logout");
   }
+
+
+
+  useEffect(() => {
+    async function fetchUserListings() {
+
+      const listingRef = collection(database, "RealtorCloneListing");
+
+      const q = query(listingRef, where("useRef", "==", auth.currentUser.uid),
+        orderBy("timestampt", "desc")
+      );
+
+      const querySnap = await getDoc(q);
+
+      let listing = [];
+
+      querySnap.forEach((doc) => {
+        return listing.push({
+          id: doc.id,
+          data: doc.data(),
+        });
+      });
+
+      setListings(listings);
+
+      setLoading(false);
+
+    }
+
+    fetchUserListings();
+  }, [listings]);
+
+
 
   return (
     <>
